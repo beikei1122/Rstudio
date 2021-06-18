4cfdd383a7caed890be46c0c708d1e63e0a581ec
Biqi <by663@nyu.edu>
2021-06-18 13:27:21 -0400

    June 18th

---
+++ New Project/China Project.R
@@ -0,0 +1,14 @@
+install.packages("tidyverse")
+library(tidyverse)
+install.packages("ngramr")
+library(ngramr)
+ #1-IV "China threats"# 
+ #2-IV "China Challenges"# 
+ #3-IV "China influences" #
+ #1_DV China's military power, China's military, China's military capability, China's military modernization, China's military build-up, China's military potential #
+#2_DV China's economic growth, China's economic power, China's economic strength, China's capability, China's potential#
+#3_DV China's political system, China's political structure, China's political power, China's political strength, China's political influence.#
+#4_DV China's ideology, China's socialism, China's communism, China=>undemocratic, China=>authoritarian, China's human right#
+ngram(phrase="", corpus="eng_2019", year_start= , year_end= )
+mutate(df, name = colum)
+cor.test(~a+b, data=)
